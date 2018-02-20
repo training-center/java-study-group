d1468f895933c04a236ada03a1a2cab7f808697e
Atila Augusto dos Santos <atila.sistemas@gmail.com>
2018-02-20 20:00:22 -0300

    inicial exemplos de código java

---
+++ exemplos-codigo/iniciante/orientacao-a-objetos/src/main/java/com/github/studygroup/orientacao_a_objetos/App.java
@@ -0,0 +1,13 @@
+package com.github.studygroup.orientacao_a_objetos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
